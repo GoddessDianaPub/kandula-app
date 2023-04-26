1a9549aaa67bb4e4b33bb14124ae164e3a6c78e6
GoddessDianaTop <88986177+DianaCohen@users.noreply.github.com>
2023-04-26 10:21:41 +0300

    added instances_data = sorted

--- app/src/services/instance_data.py
+++ app/src/services/instance_data.py
@@ -45,5 +45,9 @@ class InstanceData:
                     instance_data['NetworkInterfaceId'] = instance['NetworkInterfaces'][0]['NetworkInterfaceId']
 
                 instances_data.append(instance_data)
+                
+        # Sort the instances by the value of the "Name" tag
+        instances_data = sorted(instances_data, key=lambda i: [t['Value'] for t in i['Tags'] if t['Key'] == 'Name'])
+        
         return {'Instances': instances_data}
     
