0a6669b1a620b16de44928224b25c67ab985bd6f
DianaCohen <ac171@walla.com>
2023-06-13 00:11:47 +0300

    ordered the folders and files

--- nginx-ingress-controller-manifests/calenaup_nginx.sh
+++
@@ -1,5 +0,0 @@
-kubectl delete clusterrole ingress-nginx
-kubectl delete clusterrolebinding ingress-nginx
-kubectl delete ValidatingWebhookConfiguration ingress-nginx-admission
-kubectl delete ns ingress-nginx
-sleep 5
