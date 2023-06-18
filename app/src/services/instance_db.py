bd19bcfca2328bbf43f6c5963b2ea816d304fec9
DianaCohen <ac171@walla.com>
2023-06-18 21:52:40 +0300

    changed the code

--- app/src/services/instance_db.py
+++ app/src/services/instance_db.py
@@ -1,98 +1,85 @@
 import json
 import psycopg2
 
+# Variables
+host = "rds-db-instance-0.cihzevxi90ql.us-east-1.rds.amazonaws.com"
+port = 5432
 schema = "kandula"
-table_name = "kandula"
+database = "kandula"
+scheduler_table = "instances_scheduler"
+log_table = "instances_shutdown_log"
+user = "kandula"
+password = "Aa123456!"
+
 con = psycopg2.connect(
-    host="rds-db-instance-0.cihzevxi90ql.us-east-1.rds.amazonaws.com",
-    port=5432,
-    database="kandula",
-    user="kandula",
-    password="Aa123456!"
+    host=host,
+    port=port,
+    database=database,
+    user=user,
+    password=password
 )
 
-def check_postgresql_connection():
-    try:
-        con = psycopg2.connect(
-            host="rds-db-instance-0.cihzevxi90ql.us-east-1.rds.amazonaws.com",
-            port=5432,
-            database="kandula",
-            user="kandula",
-            password="Aa123456!"
-        )
-        con.close()
-        print("PostgreSQL connection successful!")
-    except psycopg2.Error as e:
-        print("Unable to connect to PostgreSQL:", e)
 
-# Call the function to check the connection
-check_postgresql_connection()
-
-
-def insert_data(con):
+def insert_data(con, scheduler_table):
     try:
         cur = con.cursor()
-        if table_name == "not set":
+        if table_name == "instances_scheduler":
             print("Table not created yet")
         else:
             id_val = input("Enter the value for id: ")
             name_val = input("Enter the value for name: ")
             cur.execute(
-                f"INSERT INTO {schema}.{table_name} (id, name) VALUES (%s, %s)",
+                f"INSERT INTO {schema}.{scheduler_table} (id, name) VALUES (%s, %s)",
                 (id_val, name_val),
             )
-            cur.close()
             con.commit()
+            print("Data inserted successfully")
+        cur.close()
     except Exception as error:
         print("ERROR:", error)
-        cur.close()
-        con.commit()
 
 
-def create_table(con):
+def query_table(con, scheduler_table):
     try:
         cur = con.cursor()
-        global table_name
-        table_name = input("Choose a table name: ")
-        cur.execute(f"CREATE TABLE {schema}.{table_name}(id INT PRIMARY KEY, name VARCHAR(50))")
+        if table_name == "instances_scheduler":
+            print("Table not created yet")
+        else:
+            cur.execute(f"SELECT id, name FROM {schema}.{scheduler_table}")
+            rows = cur.fetchall()
+            print("")
+            print("{:<8} {:<15}".format("id", "name"))
+            print("----     -----")
+            for r in rows:
+                print("{:<8} {:<15}".format(r[0], r[1]))
         cur.close()
-        con.rollback()
-    except psycopg2.errors.DuplicateTable as error:
-        cur.close()
-        con.commit()
+    except Exception as error:
         print("ERROR:", error)
-        use_table_name = input(f"Would you like to use {table_name} table? (y/n)")
-        if use_table_name != "y":
-            table_name = "not set"
 
 
-def query_table(con):
-    if table_name == "not set":
-        print("Table not created yet")
-    else:
+def delete_from_table(con, scheduler_table):
+    try:
         cur = con.cursor()
-        cur.execute(f"SELECT id, name FROM {schema}.{table_name}")
-        rows = cur.fetchall()
-        print("")
-        print("{:<8} {:<15}".format("id", "name"))
-        print("----     -----")
-        for r in rows:
-            print("{:<8} {:<15}".format(r[0], r[1]))
+        if table_name == "instances_scheduler":
+            print("Table not created yet")
+        else:
+            id_val = input("Enter the id to delete: ")
+            cur.execute(f"DELETE FROM {schema}.{scheduler_table} WHERE id = %s", (id_val,))
+            con.commit()
+            print("Data deleted successfully")
         cur.close()
-
-
-def delete_from_table(con):
-    print("Not implemented yet")
+    except Exception as error:
+        print("ERROR:", error)
 
 
 def leave(con):
     con.close()
 
 
-def get_scheduling(con, schema, table_name):
+def get_scheduling(con, schema, scheduler_table):
     try:
         cur = con.cursor()
-        cur.execute(f"SELECT instance_id, shutdown_time FROM {schema}.{table_name}")
+        cur.execute(f"SELECT instance_id, shutdown_time FROM {schema}.{scheduler_table}")
         rows = cur.fetchall()
         cur.close()
 
@@ -116,41 +103,88 @@ def get_scheduling(con, schema, table_name):
         print("ERROR:", error)
 
 
-
-
-def create_scheduling(os_type, shutdown_hour):
-    instance_schedule = json.loads(get_scheduling())
-    try:  # Update
-        index = next(
+def create_scheduling(shutdown_hour, scheduler_table):
+    instance_schedule = get_scheduling(con, schema, scheduler_table)
+    try:  # Update 22:00
+        index_22 = next(
             index
             for index, instance in enumerate(instance_schedule["Instances"])
-            if instance["OSType"] == os_type
+            if instance["Stop"] == "22:00"
         )
-        instance_schedule["Instances"][index]["DailyShutdownHour"] = int(shutdown_hour)
+        instance_schedule["Instances"][index_22]["DailyShutdownHour"] = int(shutdown_hour)
         print(
-            "Instances with OS type '{}' will be shutdown at {}".format(
-                os_type, shutdown_hour
+            "Instances with Stop time '22:00' will be shutdown at {}".format(
+                shutdown_hour
             )
         )
-    except StopIteration:  # Insert
+    except StopIteration:  # Insert 22:00
         instance_schedule["Instances"].append(
-            {"OSType": os_type, "DailyShutdownHour": int(shutdown_hour)}
+            {"Stop": "22:00", "DailyShutdownHour": int(shutdown_hour)}
         )
         print(
-            "Instances with OS type '{}' will be shutdown every day at {}".format(
-                os_type, shutdown_hour
+            "Instances with Stop time '22:00' will be shutdown every day at {}".format(
+                shutdown_hour
             )
         )
-    # TODO: Implement a DB insert that creates the OS type and the chosen hour in the DB
+
+    try:  # Update 23:00
+        index_23 = next(
+            index
+            for index, instance in enumerate(instance_schedule["Instances"])
+            if instance["Stop"] == "23:00"
+        )
+        instance_schedule["Instances"][index_23]["DailyShutdownHour"] = int(shutdown_hour)
+        print(
+            "Instances with Stop time '23:00' will be shutdown at {}".format(
+                shutdown_hour
+            )
+        )
+    except StopIteration:  # Insert 23:00
+        instance_schedule["Instances"].append(
+            {"Stop": "23:00", "DailyShutdownHour": int(shutdown_hour)}
+        )
+        print(
+            "Instances with Stop time '23:00' will be shutdown every day at {}".format(
+                shutdown_hour
+            )
+        )
+    # TODO: Implement a DB insert that creates the instance schedule in the table
     # Remember to store the updated instance_schedule back in the database
 
 
-def delete_scheduling(os_type):
-    # Add indentation here
+
+def delete_scheduling(con, scheduler_table):
     try:
-        # Delete scheduling logic
-        pass
+        cur = con.cursor()
+        cur.execute(f"DELETE FROM {schema}.{scheduler_table}")
+        con.commit()
+        print("Scheduling deleted successfully")
+        cur.close()
+    except Exception as error:
+        print("ERROR:", error)
+        
+
+
+def insert_log(con, schema, log_table, instance_id, shutdown_time):
+    try:
+        cur = con.cursor()
+        cur.execute(
+            f"INSERT INTO {schema}.{log_table} (instance_id, shutdown_time) VALUES (%s, %s)",
+            (instance_id, shutdown_time),
+        )
+        con.commit()
+        cur.close()
+        print("Log inserted successfully")
     except Exception as error:
         print("ERROR:", error)
 
 
+def delete_log(con, schema, log_table):
+    try:
+        cur = con.cursor()
+        cur.execute(f"DELETE FROM {schema}.{log_table}")
+        con.commit()
+        print("Logs deleted successfully")
+        cur.close()
+    except Exception as error:
+        print("ERROR:", error)
