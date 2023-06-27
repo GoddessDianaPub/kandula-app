9ca5360bee73c128b7958de76b93aeb7f0dc96e4
DianaCohen <ac171@walla.com>
2023-06-27 11:50:01 +0300

    changed metrics

--- app/src/services/metrics.py
+++ app/src/services/metrics.py
@@ -1,8 +1,8 @@
-import prometheus_client
+from prometheus_client import Counter, Gauge
 from flask import Flask, render_template
 
 app = Flask(__name__)
-page_hits_counter = Counter('kandula_page_hits_total', 'Number of times each page was hit')
+page_hits_counter = Counter('kandula_page_hits', 'Number of times each page was hit')
 inprogress_requests_gauge = Gauge('kandula_inprogress_requests', 'Number of in-progress requests')
 
 def handle_page_request(page):
@@ -12,6 +12,8 @@ def handle_page_request(page):
 
 def process_request(page):
     inprogress_requests_gauge.inc()
+
+    inprogress_requests_gauge.dec()
 
 @app.route('/')
 def home():
@@ -42,25 +44,6 @@ def health():
 def about():
     handle_page_request('About')
     return 'About Page'
-    
-import prometheus_client
-
-@app.route('/dashboard')
-def dashboard():
-    prometheus_metrics = prometheus_client.generate_latest()
-    parsed_metrics = prometheus_client.parser.text_string_to_metric_families(prometheus_metrics)
-
-    metrics_dict = {}
-    for metric in parsed_metrics:
-        metric_name = metric.name
-        metric_data = {}
-        for sample in metric.samples:
-            labels = {key: value for key, value in sample.labels.items()}
-            metric_data[sample.name] = (sample.value, labels)
-        metrics_dict[metric_name] = metric_data
-
-    return render_template('dashboard.html', prometheus_metrics=metrics_dict, title='Dashboard')
-
 
 if __name__ == '__main__':
     app.run()
